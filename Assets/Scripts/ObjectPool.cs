bbd8366eb0a3abbd426e698f24d466cb26d4db96
Seo-Woongbi <suwoongby@naver.com>
2024-10-25 12:47:40 +0900

    Update Standard Q2.[1]

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,32 +1,47 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
     public GameObject prefab;
     private List<GameObject> pool = new List<GameObject>();
     public int poolSize = 300;
-    public Dictionary<string, Queue<GameObject>> PoolDictionary;
-    private GameObject arrow;
+    //public Dictionary<string, Queue<GameObject>> PoolDictionary;
 
     void Start()
-    { 
+    {
+        GameObject obj;
         // 미리 poolSize만큼 게임오브젝트를 생성한다.
         //PoolDictionary = new Dictionary<string, Queue<GameObject>>(poolSize);
-        pool = new List<GameObject>(300);
+        for(int i = 0; i < poolSize; i++)
+        {
+            obj = Instantiate(prefab);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
     }
 
     public GameObject Get()
     {
         // 꺼져있는 게임오브젝트를 찾아 active한 상태로 변경하고 return 한다.
-        for (int i = 0; i < pool.Count; i++)
+        for (int i = 0; i < poolSize; i++)
         {
-            pool[i].SetActive(true);            
+            if (!pool[i].activeInHierarchy)
+            {
+                pool[i].SetActive(true);
+                return pool[i];
+            }
         }
+        return null;
     }
 
     public void Release(GameObject obj)
     {
         // 게임오브젝트를 deactive한다.
+        for(int i = 0; i < poolSize; i++)
+        {
+            obj.SetActive(false);
+        }
     }
 }
--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -19,18 +19,6 @@ public class QuestManager : MonoBehaviour
 
     private void Awake()
     {
-        instance = this;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(instance == null) instance = this;
     }
 }
